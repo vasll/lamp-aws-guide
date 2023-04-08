515f2c6dfc9dbeda9d4211629a09eec6982926aa
vasll <valentinosoll17@gmail.com>
2023-04-08 19:13:18 +0200

    Fixed css imports

--- js/https-redirector.js
+++
@@ -1,6 +0,0 @@
-if (window.location.protocol == 'http:') {
-    console.log("HTTP detected | Redirecting to HTTPS...")
-    window.location.href = window.location.href.replace('http:', 'https:')
-}else if(window.location.protocol == "https:") {
-    console.log("HTTPS detected. Everything is good")
-}
